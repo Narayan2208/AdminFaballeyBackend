b2ff533e85d58f7971b4c80674f8faa62fceeaef
narayan dutta <narayandutta2208@gmail.com>
2023-04-18 20:16:10 +0530

    Dashboard

---
+++ Dashboard/src/component/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Stack, Button,HStack, VStack, Box } from '@chakra-ui/react'
+function Sidebar() {
+  return (
+    <>
+  
+        <Box display={"flex"} flexDirection={"column"}>
+        <Button>User</Button>
+        <Button>Products</Button>
+        <Button>Products</Button>
+        <Button>Products</Button>
+        <Button>Products</Button>
+        <Button>Products</Button>
+        </Box>
+      
+    </>
+  )
+}
+
+export default Sidebar
